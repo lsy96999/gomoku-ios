6f7aecdaf56703f10e9b7f862061ad2fb4ea48aa
lsy96999 <lsy96999@gmail.com>
2022-12-24 13:28:10 +0900

    Initial Commit

---
+++ gomoku/gomokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  gomokuApp.swift
+//  gomoku
+//
+//  Created by N-312 on 2022/12/24.
+//
+
+import SwiftUI
+
+@main
+struct gomokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
